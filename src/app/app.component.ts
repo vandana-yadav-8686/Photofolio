11c537647c96ce8aa72f0eef0a064106a25652a7
Varsha <varsha2181999@gmail.com>
2023-09-18 16:36:31 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ecommerce';
+  constructor(){
+   
+  }
+  menuIcon:boolean = false;
+  menuclick(){
+    this.menuIcon =! this.menuIcon;
+    setTimeout(() => {
+      this.menuIcon = false;
+    }, 1500);
+     
+  }
 }
